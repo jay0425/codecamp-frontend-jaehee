254b3a6925fd42ad57c456197431c036595de589
jay0425 <heedev94@gmail.com>
2023-10-10 19:51:59 +0900

    first commit

---
+++ pages/section01/01-01-example/index.js
@@ -0,0 +1,11 @@
+export default function Example() {
+  const aaa = 3; // -> 위쪽은 자바스크립트 작성하는 공간
+
+  // 아래쪽은 html 작성하는 공간
+  return (
+    <div>
+      <p>철수</p>
+      <button>클릭하세요</button>
+    </div>
+  );
+}
