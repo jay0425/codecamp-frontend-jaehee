b4d20b51868876d6ba31e438bdad070a067cafbc
jay0425 <heedev94@gmail.com>
2023-11-30 20:28:57 +0900

    게시글 수정하기에서 원본 가져오기, 수정했을 때 수정하지 않은 부분  삭제되는 현상 없애기

---
+++ src/components/units/board/09-write2/BoardWrite.presenter.js
@@ -0,0 +1,23 @@
+import { RedInput, BlueButton } from './BoardWrite.styles';
+
+export default function BoardWriteUI(props) {
+  if (props.edit) {
+    console.log('데이터가 넘어왔는가' + props.data?.fetchBoard);
+  }
+  return (
+    <div>
+      <div>@@@@@@@@@@@@@@@@@ 여기는 프레젠터입니다 @@@@@@@@@@@@@@@@@</div>
+      <div>
+        작성자 : <RedInput type="text" onChange={props.onChangeWriter} defaultValue={props.data?.fetchBoard.writer} />
+        제목 : <RedInput type="text" onChange={props.onChangeTitle} defaultValue={props.data?.fetchBoard.title} />
+        내용 : <RedInput type="text" onChange={props.onChangeContents} defaultValue={props.data?.fetchBoard.contents} />
+        <BlueButton onClick={props.isEdit ? props.onClickUpdate : props.onClickSubmit} isActive={props.isActive}>
+          {props.isEdit ? '수정' : '등록'}하기
+        </BlueButton>
+      </div>
+      <div>@@@@@@@@@@@@@@@@@ 여기는 프레젠터입니다 @@@@@@@@@@@@@@@@@</div>
+    </div>
+  );
+}
+
+export const apple = 3;
