248faa400be2f9189d5a6fbc1c86dc2df6529b16
jay0425 <heedev94@gmail.com>
2023-12-17 08:58:21 +0900

    타입스크립트 utility type

--- pages/section10/10-03-typescript-utility-type/index.ts
+++ pages/section10/10-03-typescript-utility-type/index.ts
@@ -6,25 +6,30 @@ export interface IProfile {
 }
 
 // 1. Partial 타입
+// 모든 타입 물음표
 type aaa = Partial<IProfile>;
 
 // 2. Required 타입
+// 모든 물음표 삭제
 type bbb = Required<IProfile>;
 
 // 3. Pick 타입
+// IProfile에서 name과 age만 '고르기'
 type ccc = Pick<IProfile, 'name' | 'age'>;
 
 // 4. Omit 타입
+// IProfile에서 school만 '제외'
 type ddd = Omit<IProfile, 'school'>;
 
 // 5. Record 타입
-type eee = '철수' | '영희' | '훈이'; // Union 타입
+type eee = '철수' | '영희' | '훈이'; // Union 타입 (합집합)
 let child1: eee = '영희'; // 철수, 영희, 훈이만 됨
 let child2: string = '사과'; // 철수, 영희, 훈이, 사과, 바나나 다 됨
 
-type fff = Record<eee, IProfile>; // Record 타입
+type fff = Record<eee, IProfile>; // Record 타입 <키, 벨류>
 
 // 6. 객체의 key들로 Union 타입 만들기
+// IProfile에서 키들만 빼오는 것
 type ggg = keyof IProfile; // "name" | "age" | "school" | "hobby"
 let myprofile: ggg = 'hobby';
 
